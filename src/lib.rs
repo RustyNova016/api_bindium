eb92c2f77057a751ba2a577962e525d4bc16934a
RustyNova <rusty.nova.jsb@gmail.com>
2026-01-15 14:41:55 +0100

    chore!: reexport governor as a module

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,6 @@ pub use crate::http_verb::HTTPVerb;
 pub use api_request::ApiRequest;
 
 // === Re-exports ===
-
 #[cfg(feature = "rate_limit")]
-pub use governor::*;
+pub use governor;
 pub use ureq;
