52cf57858cc4a9023f0a59ba3910700e06df91f1
RustyNova <rusty.nova.jsb@gmail.com>
2026-08-20 10:52:38 +0200

    fix: sleep_until not working

--- src/utils.rs
+++ src/utils.rs
@@ -3,12 +3,10 @@ use std::time::Instant;
 
 #[cfg(feature = "sync")]
 pub(crate) fn sleep_until(until: Instant) {
-    let dur = until.elapsed();
-    if dur.as_secs() > 0 {
-        use std::thread::sleep;
+    use std::thread::sleep;
 
-        sleep(dur);
-    }
+    let dur = until - Instant::now();
+    sleep(dur);
 }
 
 #[cfg(feature = "async")]
@@ -16,4 +14,40 @@ pub(crate) async fn sleep_until_async(until: Instant) {
     use async_io::Timer;
 
     Timer::at(until).await;
+}
+
+#[cfg(test)]
+mod test {
+    #[cfg(feature = "async")]
+    use macro_rules_attribute::apply;
+
+    #[cfg(feature = "sync")]
+    #[test]
+    fn test_sleep_until() {
+        use core::time::Duration;
+        use std::time::Instant;
+
+        use crate::utils::sleep_until;
+
+        let now = Instant::now();
+        let then = now + Duration::from_secs(1);
+        sleep_until(then);
+        let now = Instant::now();
+        assert!(now > then);
+    }
+
+    #[cfg(feature = "async")]
+    #[apply(smol_macros::test!)]
+    async fn test_sleep_until_async() {
+        use core::time::Duration;
+        use std::time::Instant;
+
+        use crate::utils::sleep_until_async;
+
+        let now = Instant::now();
+        let then = now + Duration::from_secs(1);
+        sleep_until_async(then).await;
+        let now = Instant::now();
+        assert!(now > then);
+    }
 }
